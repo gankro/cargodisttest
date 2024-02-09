d94bc4807cfe2a04fe54156e151466b900ff5f6d
Aria Beingessner <a.beingessner@gmail.com>
2024-02-09 11:57:35 -0500

    0.4.1 release

--- dist-stage.ps1
+++ dist-stage.ps1
@@ -3,14 +3,14 @@
 $env:STAGE_INTO_THE_ABYSS=1
 
 # install latest cargo-dist (should make this optional/configurable)
-irm https://github.com/axodotodev/cargo-dist/releases/latest/download/cargo-dist-installer.ps1 | iex
+irm https://github.com/axodotodev/cargo-dist/releases/download/latest/cargo-dist-installer.ps1 | iex
 
 # run cargo-dist as lies
 echo "reconfiguring to push to axo staging"
 cargo dist init -y --hosting=axodotdev
 echo "building + releasing to staging"
-cargo dist host --steps=create
-cargo dist build --artifacts=lies
-cargo dist host --steps=upload --steps=release --steps=announce
+cargo dist host --steps=create --tag=cargodisttest-v0.4.1
+cargo dist build --artifacts=lies --tag=cargodisttest-v0.4.1
+cargo dist host --steps=upload --steps=release --steps=announce --tag=cargodisttest-v0.4.1
 
 echo "done! (you might want to git clean/stash the changes we just made)"
