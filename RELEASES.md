2e38227bc419572b561847cab708e4001118ba20
Aria Beingessner <a.beingessner@gmail.com>
2024-02-21 12:25:01 -0500

    release: 0.4.4

--- RELEASES.md
+++ RELEASES.md
@@ -1,3 +1,11 @@
+# Version 0.4.4 (2024-02-21)
+
+These are some great release notes!!!
+
+## HASH TAG WOW
+
+WHAT A RELEASE!
+
 # Version 0.2.0 (2023-04-24)
 
 This is the first crates.io published release of cargodisttest, to test release workflows
--- dist-stage.ps1
+++ dist-stage.ps1
@@ -2,8 +2,11 @@
 # NOTE: it is assumed that AXO_RELEASES_STAGING_TOKEN is set by the caller!
 $env:STAGE_INTO_THE_ABYSS=1
 
+# clean up previous runs
+rm -Force target/distrib/dist-manifest.json
+
 # install latest cargo-dist (should make this optional/configurable)
-irm http://github.com/axodotdev/cargo-dist/releases/latest/download/cargo-dist-installer.ps1 | iex
+irm https://github.com/axodotodev/cargo-dist/releases/latest/download/cargo-dist-installer.ps1 | iex
 
 # run cargo-dist as lies
 echo "reconfiguring to push to axo staging"
